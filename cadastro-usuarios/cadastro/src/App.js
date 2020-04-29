06f143817d1cfde82ebd6c4bf911082fe419fec3
Fe <zecchinfernanda@yahoo.com.br>
2020-04-29 16:48:40 -0300

    alterações e inclusão de mais um componente

--- cadastro-usuarios/cadastro/src/App.js
+++ cadastro-usuarios/cadastro/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import {ListaUsuario} from './components/ListaUsuario'
+import axios from 'axios';
+
 
 const FormContainer = styled.div`
   border: 2px solid black;
@@ -19,19 +22,72 @@ const BotaoSalvar = styled.button`
 `
 
 
-
 class App extends React.Component {
+
+   state = {
+     valorNome: '',
+     valorEmail: '',
+     mudaPagina: 'cadastro'
+   };
+   
+  criarUsuario = () =>{
+    const body = {
+      name: this.state.valorNome,
+      email: this.state.valorEmail
+    }
+    axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users', body, {
+      headers: {
+        Authorization: 'fernanda-zecchin-julian'
+      }
+    }).then((resposta) => {
+      window.alert("deu certo")
+    }).catch((error) => {
+      window.alert("deu erro")
+    })
+    this.setState({valorNome: "", valorEmail:""})
+  }
+  
+
+   onChangevalorNome = (event) => {
+     this.setState({valorNome: event.target.value})
+   };
+
+   onChangevalorEmail = (event) => {
+     this.setState({valorEmail: event.target.value})
+   };
+
+  
+   
+  
+  onClickmudarPag = () => {
+     const proxPagina = this.state.mudaPagina === 'cadastro' ? 'lista' : 'cadastro'
+     this.setState({mudaPagina: proxPagina})
+     
+  }
+
+
+
   render () {
     return (
       <div>
-      < FormContainer>
+      <FormContainer>
         <h1>Cadastro</h1>
         <p>Nome:</p>
-        <input/>
+        <input 
+        placeholder={"Seu nome"}
+        onChange={this.onChangevalorNome}
+        value={this.state.valorNome}
+        />
         <p>E-mail:</p>
-        <input/>
-        <BotaoSalvar>Salvar</BotaoSalvar>
-        <BotaoSalvar>Ir para página de lista</BotaoSalvar>
+        <input
+        placeholder={'Seu e-mail'}
+        onChange={this.onChangevalorEmail}
+        value={this.state.valorEmail}
+        />
+        <BotaoSalvar onClick={this.criarUsuario}>Salvar</BotaoSalvar>
+        <BotaoSalvar onClick={this.onClickmudarPag}>Ir para página de lista</BotaoSalvar>
+        
+       
       </ FormContainer>
       </div>
     )
