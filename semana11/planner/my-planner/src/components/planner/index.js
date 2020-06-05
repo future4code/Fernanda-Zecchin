b7e00f09954492d25f4d6f53e5e391ff76e94ce7
Fe <zecchinfernanda@yahoo.com.br>
2020-06-05 17:06:02 -0300

    realizei apenas dois testes, acompanhando a aula

--- semana11/planner/my-planner/src/components/planner/index.js
+++ semana11/planner/my-planner/src/components/planner/index.js
@@ -1,18 +1,63 @@
-import React, {useState} from "react";
-import {ContainerCriarTarefa, ContainerDiasDaSemana} from "./style"
+import React, {useState, useEffect} from "react";
+import {ContainerCriarTarefa, ContainerDiasDaSemana, Semana} from "./style";
+import axios from 'axios';
 
 
 
  function Planner() {
-   const[adicionarTarefa, setAdicionarTarefa] = useState("");
+   const[inputTarefa, setInputTarefa] = useState('');
+   const[selecionaSemana, setSelecionaSemana] = useState('');
+   const[listaTarefas, setListaTarefas] = useState([]);
+   
+   const buscarListaTarefas = () => {
+    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-julian-fernanda-zecchin').then((response) => {
+     setListaTarefas(response.data)
+    })
+   }
 
-   const onChangeTarefa = event => {
-    setAdicionarTarefa(event.target.value);
+   useEffect(() => {
+    buscarListaTarefas()
+
+    }, [])
+
+      
+   const onChangeInputTarefa = event => {
+    setInputTarefa(event.target.value);
+   };
+   
+   const onChangeSelecionaSemana = event => {
+    setSelecionaSemana(event.target.value);
    };
 
-   const onClickAdicionar = () => {
-     console.log(adicionarTarefa)
+   const adicionarTarefa = () => {
+     const body = {
+       text:inputTarefa,
+       day: selecionaSemana
+     }
+     axios.post('https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-julian-fernanda-zecchin', body,
+     {
+       headers: {aluno: 'planner-julian-fernanda-zecchin',
+      }
+     }).then((response) => {
+       console.log(response.data)
+     }).catch((error) => {
+       console.log(error)
+     })
+     buscarListaTarefas()
    }
+  
+  
+  //  const renderizaTarefa = listaTarefas.filter((tarefa) => {
+  //    return tarefa.day === titulo
+  //  }).map((tarefa,index) => {
+  //    return <ContainerDiasDaSemana id={tarefa.id} key={index}/>
+  //  })
+  
+ 
+  
+ 
+
+ 
 
   return (
     <div>
@@ -20,35 +65,96 @@ import {ContainerCriarTarefa, ContainerDiasDaSemana} from "./style"
         <input
           type="text"
           placeholder="adicione sua tarefa"
-          value={adicionarTarefa}
-          onChange={onChangeTarefa}
+          value={inputTarefa}
+          onChange={onChangeInputTarefa}
         />
 
-          <select>
+          <select value={selecionaSemana} onChange={onChangeSelecionaSemana}>
             <option value=""></option>
-            <option value="segunda">Segunda</option>
-            <option value="terca">Terça</option>
-            <option value="quarta">Quarta</option>
-            <option value="quinta">Quinta</option>
-            <option value="sexta">Sexta</option>
-            <option value="sabado">Sábado</option>
-            <option value="domingo">Domingo</option>
+            <option value={'Segunda'}>Segunda</option>
+            <option value={"Terca"}>Terça</option>
+            <option value={"Quarta"}>Quarta</option>
+            <option value={"Quinta"}>Quinta</option>
+            <option value={"Sexta"}>Sexta</option>
+            <option value={"Sabado"}>Sábado</option>
+            <option value={"Domingo"}>Domingo</option>
           </select>
 
-          <button onClick={onClickAdicionar}>Criar Tarefa</button>
+          <button onClick={adicionarTarefa} data-testid={'botao-adicionar'}>Criar Tarefa</button>
 
       </ContainerCriarTarefa>
 
-          <ContainerDiasDaSemana>
-            <div>Segunda</div>
-            <div>Terça</div>
-            <div>Quarta</div>
-            <div>Quinta</div>
-            <div>Sexta</div>
-            <div>Sábado</div>
-            <div>Domingo</div>
+      <ContainerDiasDaSemana>  
+
+            <Semana>
+              <h4>Domingo</h4>
+              {listaTarefas.filter(tarefa => tarefa.day === "Domingo").map(filteredTarefa => (
+               <p>
+                 {filteredTarefa.text}
+               </p>
+             ))}
+            </Semana>
+        
+       
+            <Semana>
+             <h4>Segunda</h4> 
+             {listaTarefas.filter(tarefa => tarefa.day === "Segunda").map(filteredTarefa => (
+               <p>
+                 {filteredTarefa.text}
+               </p>
+             ))}
+            </Semana>
+
+            <Semana>
+              <h4>Terça</h4>
+              {listaTarefas.filter(tarefa => tarefa.day === "Terça").map(filteredTarefa => (
+               <p>
+                 {filteredTarefa.text}
+               </p>
+             ))}
+            </Semana>
+
+            <Semana>
+              <h4>Quarta</h4>
+              {listaTarefas.filter(tarefa => tarefa.day === "Quarta").map(filteredTarefa => (
+               <p>
+                 {filteredTarefa.text}
+               </p>
+             ))}
+            </Semana>
+
+            <Semana>
+              <h4>Quinta</h4>
+              {listaTarefas.filter(tarefa => tarefa.day === "Quinta").map(filteredTarefa => (
+               <p>
+                 {filteredTarefa.text}
+               </p>
+             ))}
+            </Semana>
+
+            <Semana>
+              <h4>Sexta</h4>
+              {listaTarefas.filter(tarefa => tarefa.day === "Sexta").map(filteredTarefa => (
+               <p>
+                 {filteredTarefa.text}
+               </p>
+             ))}
+            </Semana>
+
+            <Semana>
+              <h4>Sábado</h4>
+              {listaTarefas.filter(tarefa => tarefa.day === "Sábado").map(filteredTarefa => (
+               <p>
+                 {filteredTarefa.text}
+               </p>
+             ))}
+            </Semana>
+
+
+         
+         
           </ContainerDiasDaSemana>  
-    </div>
+   </div>
   )
 }
 export default Planner;
--- semana11/planner/my-planner/src/components/planner/style.js
+++ semana11/planner/my-planner/src/components/planner/style.js
@@ -4,12 +4,21 @@ export const ContainerCriarTarefa = styled.div`
   display:flex;
   justify-content:space-evenly;
   margin-top: 1rem;
+  border-bottom: 1px  solid black;
+  padding-bottom: 1rem;
 `
 
 export const ContainerDiasDaSemana = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, 100px);
+  grid-template-columns: repeat(7, 1fr);
   justify-content:center;
-  margin-top: 1rem;
+  margin: 1rem;
+  column-gap: 1rem;
+  
 
+`
+export const Semana = styled.div`
+  border-right: 1px solid black;
+  min-height: 80vh;
+  
 `
